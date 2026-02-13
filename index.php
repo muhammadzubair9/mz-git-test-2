05e40e660f9b3a6d0a1307d7a96526856003142d
muhammadzubair9 <muhammad.zubair912006@gmail.com>
2026-02-13 15:41:00 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut totam optio laborum, mollitia consequuntur itaque aliquam cumque aspernatur magni. Nobis similique vero quidem deserunt, consequuntur blanditiis qui tenetur? Ad, eum.</p>
 </body>
 </html>
